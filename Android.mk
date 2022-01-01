sonyxperiadev/hardware-qcom-display

build: add barrier for kernel version
The display, media and camera have to be in sync with the binary release and kernel.

Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(TARGET_DISABLE_DISPLAY),true)
+ifneq ($(filter 4.14, $(SOMC_KERNEL_VERSION)),)
 
 display-hals := \
     include\

moto-common/platform_vendor_qcom_opensource_display_sm8150

Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(TARGET_DISABLE_DISPLAY),true)
+ifneq ($(filter 4.14, $(KERNEL_VERSION)),)
 
 display-hals := \
     include\